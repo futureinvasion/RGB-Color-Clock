d3882f62895d854c20fc0e46be9142ebd1f400c1
Pete <Pete@Petes-MacBook-Pro.local>
2014-12-17 11:00:49 +0700

    Initial Commit

---
+++ ColorClock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorClock
+//
+//  Created by Pete on 12/17/2557 BE.
+//  Copyright (c) 2557 Pete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

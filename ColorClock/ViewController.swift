4fc15b9294e7756f6dbf3f8987c737d0cefb585c
Pete <Pete@Petes-MacBook-Pro.local>
2014-12-17 15:13:13 +0700

    Added Code

--- ColorClock/ViewController.swift
+++ ColorClock/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var clockLabel: UILabel!
+    @IBOutlet var colorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        clockLabel.textColor = UIColor.whiteColor()
+        clockLabel.font = UIFont(name: "OpenSans-Light", size: 80.0)
+        colorLabel.textColor = UIColor.whiteColor()
+        colorLabel.font = UIFont(name: "OpenSans-Light", size: 20.0)
+        
+        // Update Clock
+        updateCurrentClock()
+        NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "updateCurrentClock", userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func updateCurrentClock() {
+        let date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.CalendarUnitHour | .CalendarUnitMinute | .CalendarUnitSecond, fromDate: date)
+        let currentTime = NSString(format:"%02d : %02d : %02d", components.hour, components.minute, components.second) as String
+        let currentColor = NSString(format:"#%02d%02d%02d", components.hour, components.minute, components.second) as String
+        clockLabel.text = currentTime
+        colorLabel.text = currentColor
+        view.backgroundColor = UIColor(rgba: currentColor)
+    }
 }
 
